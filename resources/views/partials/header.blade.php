4c9b83c7974348557762c11c28b801b5ee800ee6
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-10 15:59:59 +0200

    prova inserimento immagini

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,21 @@
 <header>
-    <img src="" alt="Logo">
-    <nav>
-        <ul>
-            <li><a href="{{route}}">Home</a></li>
-        </ul>
-    </nav>
+<header>
+  <div class="logo">
+    <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo">
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="">characters</a></li>
+      <li><a href="">comics</a></li>
+      <li><a href="">movies</a></li>
+      <li><a href="">Tv</a></li>
+     
+    </ul>
+  </nav>
+</header>
+
+<div class="jumbotron">
+  <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="jumbotron" />
+</div>
 </header>
